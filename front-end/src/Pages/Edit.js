1ab2cb774b0df891d1e2544a6e5032f6b548d462
MaxWattan <mcwattanac@gmail.com>
2023-05-09 18:47:32 -0400

    node mode removed from commit

---
+++ front-end/src/Pages/Edit.js
@@ -0,0 +1,10 @@
+// import VideoGameEditForm from "../Components/VideoGameEditForm"
+
+export default function Edit() {
+  return (
+    <div>
+       <h1>Edit</h1> 
+        {/* <VideoGameEditForm /> */}
+    </div>
+  )
+}
