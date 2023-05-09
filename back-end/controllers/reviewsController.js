e345ed247170fc02f5d37bcd03c397df14869416
Angel <EMAIL>
2023-05-08 21:30:08 -0400

    back-end finished

--- back-end/controllers/reviewsController.js
+++ back-end/controllers/reviewsController.js
@@ -1,72 +1,57 @@
 const express = require('express');
-// const review = express.Router();
-// const { getBookmark } = require('../queries/bookmarks')
-const review = express.Router({ mergeParams: true });
-const { 
-    getAllReviews, 
-    getAReview, 
-    createReview, 
-    deleteReview, 
-    updateReview 
-} = require('../queries/reviews')
-
-review.get('/', async (req,res) => {
-    const { bookmarkId } = req.params;
-     try {
-        const allReviews = await getAllReviews(bookmarkId);
+const reviews = express.Router({mergeParams: true});
+const { getAllReviews, getAReview, createReview, deleteReview, updateReview } = require('../queries/reviews')
+// index route
+reviews.get("/", async (req, res) => {
+    //added below line to match frontend.  Two lines below, added bookmarkId to argument (so () to (bookmarkID))
+    const { gameId } = req.params;
+    try{
+        const allReviews = await getAllReviews(gameId)
         res.json(allReviews)
-     } catch (err) {
+    }catch (err) {
         res.json(err)
-     }
-     
-});
-
-review.get('/:id', async (req,res) => {
+    }
+  });
+// show route
+reviews.get('/:id', async (req, res) => {
     const { id } = req.params;
-    const review = await getAReview(id)
-
+    const review = getAReview(id);
     if (review) {
         res.status(200).json(review);
     } else {
-        res.status(500).json({ error: 'Server Error'})
-    }
-})
-
-// create route 
-review.post('/', async (req, res) => {
+        res.status(500).json({ error: 'Server Error'});
+    };
+});
+// create route
+reviews.post('/',  async (req, res) => {
     const newReview = req.body;
+    // const { body } = req;
     try {
-        const addedReview =  await createReview(newReview)
-        res.status(202).json(addedReview)
+        const addedReview = await createReview(newReview)
+        res.status(200).json(addedReview)
     } catch (error) {
-        res.status(400).json({ error: error })
+        res.status(400).json({ error: error})
     }
 })
-
 // delete route
-
-review.delete('/:id', async (req, res) => {
+reviews.delete('/:id', async (req, res) => {
     const { id } = req.params;
-
     try {
         const deletedReview = await deleteReview(id);
         res.status(200).json(deletedReview)
     } catch (error) {
-        res.status(400).json({ error: error })
+        res.status(400).json({ error: error})
     }
 });
-
-//update route 
-review.put('/:id', async (req, res) => {
+// update route
+reviews.put("/:id", async (req, res) => {
     const { id } = req.params;
-    const { body } = req;
-
-    try {
-       const updatedReview = await updateReview(id, body);
-       res.status(200).json(updatedReview);
-    } catch (error) {
-        res.status(400).json({ error: error});
-    };
-});
-
-module.exports = review;
+    //to update we need to know which one "id", and what we are inserting
+    const updatedReview = await updateReview(id, req.body);
+    if (updatedReview.id) {
+      res.status(200).json(updatedReview);
+    } else {
+      res.status(404).json("Review not found");
+    }
+  });
+module.exports = reviews;
