3167cc4bb11e4a01691f1e1ef96f15c6a719bbcf
Angel <EMAIL>
2023-05-09 18:28:47 -0400

    first commit on frontEnd

---
+++ front-end/src/Pages/Index.js
@@ -0,0 +1,11 @@
+import GameIndex from "../Components/GameIndex";
+function Index() {
+  return (
+    <div>
+      <h1>All Games</h1>
+      <GameIndex />
+    </div>
+  );
+}
+
+export default Index;
