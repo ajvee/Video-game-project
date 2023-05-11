2ed74dc8450352873e89b87a8e8888d8a17fd364
Angel <EMAIL>
2023-05-10 21:42:46 -0400

    added review component

---
+++ front-end/src/Components/Review.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import ReviewForm from "./ReviewForm";
+
+
+export default function Review({ review, handleDelete, handleEdit }) {
+    const [viewEditFrom, toggleEditForm] = useState(false);
+
+    const toggleView = () => {
+        toggleEditForm(!viewEditFrom);
+    };
+
+    return (
+        <div className="Review">
+            <button onClick={toggleView}>Edit this review...</button>
+            {viewEditFrom ?
+                (
+                    <ReviewForm
+                        reviewDetails={review}
+                        toggleView={toggleView}
+                        handleEdit={handleEdit} />
+                ) : (
+                    <div>
+                        <h4>{review.title} <span>{review.rating}</span></h4>
+                        <h5>{review.reviewer}</h5>
+                        <p>{review.content}</p>
+                        <button onClick={() => handleDelete(review.id)}>Delete</button>
+                    </div>
+                )}
+        </div>
+    )
+};
