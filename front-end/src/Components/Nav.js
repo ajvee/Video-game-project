63d02f721d02864133ee54d68d2da2d5366ff6cc
Angel <EMAIL>
2023-05-10 21:22:04 -0400

    better nav css

--- front-end/src/Components/Nav.js
+++ front-end/src/Components/Nav.js
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
-// import "../Css/Nav.css"
+import "../Css/Nav.css";
 
 function Nav() {
   return (
-    <nav>
-      <h1>
-        <Link to="/">4 Gamers</Link>
-      </h1>
+    <nav className="navbar">
       <ul>
+        <li>
+          <Link to="/">4 Gamers</Link>
+        </li>
+
         <li>
           <Link to="/games">View All Video Games</Link>
         </li>
